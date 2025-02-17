a9888d1af468f3520232ed936f26031e868a4ad2
Felipe-02 <80016042+Felipe-02@users.noreply.github.com>
2025-02-17 07:24:55 -0500

    Update talentotecth-app.py

--- talentotecth-app.py
+++ talentotecth-app.py
@@ -1,18 +1,18 @@
-
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns 
 import plotly.express as px
 from datetime import datetime
 
+# 1. Configuración inicial de la aplicación
 
-st. set_page_confi(
-  page_title = "Dashboard Interactivo",
-  page_icon="(°>°)",
-  layout = "wide"
+st. set_page_config(
+  page_title="Dashboard Interactivo",
+  page_icon="🔥",
+  layout="wide"
 )
 
-st.title(" (°>°) Dashboard Interactivo con Streamlit")
-st.sidebar.title(" (°-°) Opciones de Navegacion")
+st.title("🔥 Dashboard Interactivo con Streamlit")
+st.sidebar.title("🚀 Opciones de Navegación")
