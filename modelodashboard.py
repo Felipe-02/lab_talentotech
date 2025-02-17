11500ba65846e50a81ed28a0d497bf5c27899c42
Felipe-02 <80016042+Felipe-02@users.noreply.github.com>
2025-02-17 08:41:50 -0500

    Update modelodashboard.py

--- modelodashboard.py
+++ modelodashboard.py
@@ -1,1 +1,19 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import plotly.express as px
+from datetime import datetime
+
+# 1. Configuración inicial de la aplicación
+
+st. set_page_config(
+  page_title="Agroindustria en Colombia",
+  page_icon="🌳",
+  layout="wide"
+)
+
+st.title("Agroindustria en Colombia")
+st.sidebar.title(" Opciones de Visualizacion")
 
