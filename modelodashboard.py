d10469c8f3ee02f06b97523a597e61bf27a312f9
Felipe-02 <80016042+Felipe-02@users.noreply.github.com>
2025-02-18 06:49:01 -0500

    Update modelodashboard.py

--- modelodashboard.py
+++ modelodashboard.py
@@ -19,40 +19,5 @@ st.title(" 🌳 Agroindustria en Colombia 🌳")
 st.sidebar.title(" ☷ Menu Opciones")
 
 
-# Generar datos aleatorios
-num_datos = 10
 
-identificacion_proceso = random.sample(range(62, 73), num_datos)
-identificacion_producto = random.sample(range(1001, 1012), num_datos)
-
-# Generar fechas aleatorias
-def fecha_aleatoria():
-    inicio = datetime(2023, 1, 1)
-    fin = datetime(2024, 1, 1)
-    return inicio + timedelta(days=random.randint(0, (fin - inicio).days))
-
-fechas_inicio = [fecha_aleatoria() for _ in range(num_datos)]
-fechas_finalizacion = [fecha + timedelta(days=random.randint(1, 30)) for fecha in fechas_inicio]
-
-tiempo_procesamiento = [random.randint(200, 1000) for _ in range(num_datos)]
-costo_produccion = [random.randint(10000000, 20000000) for _ in range(num_datos)]
-
-# Crear DataFrame
-data = pd.DataFrame({
-    "Identificación Proceso": identificacion_proceso,
-    "Identificación Producto": identificacion_producto,
-    "Fecha de Inicio": fechas_inicio,
-    "Fecha de Finalización": fechas_finalizacion,
-    "Tiempo de Procesamiento (min)": tiempo_procesamiento,
-    "Costo de Producción ($)": costo_produccion
-})
-
-menu= st.sidebar.radio(
-  "Opciones:", 
-  ["Datos", "Visualizacion", " Configuracion"]
-   )
-
-if meu == "Datos":
-  st.subheader("Datos Generados")
-  st.dataframe(data)
 
